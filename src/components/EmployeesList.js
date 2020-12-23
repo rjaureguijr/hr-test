7f939b18929494391fd25418ccc59b71ea608b28
Rudy Jauregui <rjaureguijr@gmail.com>
2020-12-22 21:35:34 -0600

    Edits to css and list, in progress

--- src/components/EmployeesList.js
+++ src/components/EmployeesList.js
@@ -1,13 +1,28 @@
 import React from 'react';
 
 class EmployeesList extends React.Component {
+ constructor(props) {
+    super(props);
+    this.state = {
+      filterInput: ''
+    }
+  }
+  
+  filterNames( {target} ){
+    console.log(target.value);
+    this.setState({
+      filterInput: target.value
+    });
+  }
 
   render() {
     const { employees } = this.props;
+    console.log(employees);
     return (
       <React.Fragment>
         <div className="controls">
-          <input type="text" className="filter-input" data-testid="filter-input" />
+          <input type="text" className="filter-input" data-testid="filter-input" 
+          onChange={this.filterNames} />
         </div>
         <ul className="employees-list">
           { employees.map(employee => (
